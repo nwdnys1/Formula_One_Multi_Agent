1c4b3f09de67721e18636f148fb7c331ece9e7f8
ZH <147311198+nwdnys1@users.noreply.github.com>
2025-04-11 20:12:53 +0800

    Update RaceUI.cs

--- Assets/Scripts/UI/赛场/RaceUI.cs
+++ Assets/Scripts/UI/赛场/RaceUI.cs
@@ -134,16 +134,10 @@ public class RaceUI : MonoBehaviour
                 {
                     pits[i] = int.Parse(json["strategy"]["pit_stop_laps"][i].ToString());
                 }
-                string[] tyres = new string[json["strategy"]["tyre_strategy"].Count];
-                for (int i = 0; i < tyres.Length; i++)
+                string[] tyreTypes = new string[json["strategy"]["tyre_strategy"].Count];
+                for (int i = 0; i < tyreTypes.Length; i++)
                 {
-                    tyres[i] = json["strategy"]["tyre_strategy"][i].ToString();
-                }
-                // tyres解析为枚举
-                string[] tyreTypes = new string[tyres.Length];
-                for (int i = 0; i < tyres.Length; i++)
-                {
-                    tyreTypes[i] = json["strategy"]["tyre_strategy"][i].ToString();
+                    tyreTypes[i] = json["strategy"]["tyre_strategy"][i].ToString().ToLower();
                 }
                 targetCar.pitStopLaps = pits;
                 targetCar.tyreTypes = tyreTypes;
